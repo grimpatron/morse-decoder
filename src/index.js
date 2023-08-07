cca1b33e9afdaa1520c1ab84d4c4f63424d54e43
grimpatron <Hellhammer220bpm@gmail.com>
2023-08-08 02:52:16 +0300

    feat: make the task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let x;
+    let result = "";
+    let morze = ""
+    let exprLength = expr.length
+    for( let i = 0; i < exprLength; i = i + 10) {
+        x = expr.slice(0, 10);
+        expr = expr.slice(10);
+        morze = "";
+        for (let j = 0; j < x.length; j = j +2) {
+            if (x[0] == "*") {
+                morze += " ";
+                break;
+            }
+            if (x.slice(j, j + 2) == 10) {
+                morze += ".";
+            } else if (x.slice(j, j + 2) == 11) {
+                morze += "-";
+            }
+        }
+        if (morze != " ") {
+            result += MORSE_TABLE[morze];
+        } else {
+            result += " ";
+        }
+    }
+    // console.log(result);
+    return result;
 }
 
 module.exports = {
